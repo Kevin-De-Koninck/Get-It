163c257ad305cda803423f747b0a14b0dd4f1916
Kevin De Koninck <kevin.de.koninck92@gmail.com>
2018-04-17 20:15:07 +0200

    Removed pod OGCircularBar

--- Podfile
+++ Podfile
@@ -2,5 +2,4 @@ target ‘Get It’ do
   use_frameworks!
   pod 'ITSwitch'
   pod 'LetsMove'
-  pod 'OGCircularBar'
 end
--- Pods/Target Support Files/Pods-Get It/Pods-Get It-frameworks.sh
+++ Pods/Target Support Files/Pods-Get It/Pods-Get It-frameworks.sh
@@ -145,12 +145,10 @@ strip_invalid_archs() {
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/ITSwitch/ITSwitch.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/LetsMove/LetsMove.framework"
-  install_framework "${BUILT_PRODUCTS_DIR}/OGCircularBar/OGCircularBar.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/ITSwitch/ITSwitch.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/LetsMove/LetsMove.framework"
-  install_framework "${BUILT_PRODUCTS_DIR}/OGCircularBar/OGCircularBar.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
   wait
